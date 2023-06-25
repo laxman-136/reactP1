cd7a2d3a6b3d656cdd69a771735739baf480d093
[subramanyam aki] <laxmansubramanyam@gmail.com>
2023-06-25 19:31:46 +0530

    doop

--- vite-project/src/components/Hero.jsx
+++ vite-project/src/components/Hero.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import style from "../style";
+import { discount, robot } from "../assets";
 
-const Hero = () => {
-  return (
-    <div>
-
+const Hero = () => (
+  <section id="home" className={` flex md:flex-row flex-col ${style.paddingY}`}>
+    <div
+      className={`flex-1 ${style.flexStart}
+      flex-col xl:px-0 sm:px-16 px-6`}
+    >
+      <div
+        className="flex flex-row
+      items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] md-2"
+      >
+        <img src={discount} alt=""
+          className="w-[32px] h-[32px]" />
+        <p>
+          <span className="text-white ">20%</span> Discount For {" "}
+          <span className="text-white ">1 Month</span> Account
+        </p>
+      </div>
     </div>
-  )
-}
+  </section>
+);
 
-export default Hero
+export default Hero;
