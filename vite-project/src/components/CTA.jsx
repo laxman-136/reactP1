6f47943720df5798724fcc330a1ba6e8ceb9788a
[subramanyam aki] <laxmansubramanyam@gmail.com>
2023-06-29 14:01:13 +0530

    totle commit

--- vite-project/src/components/CTA.jsx
+++ vite-project/src/components/CTA.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import style from '../style'
+import Button from './Button'
 
-const CTA = () => {
-  return (
-    <div>
-      
+const CTA = () => (
+    <section className={`${style.flexCenter} ${style.marginY} ${style.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow `}>
+    <div className='flex-1 flex flex-col'>
+      <h2 className={`${style.heading2}`}>let's try our service now!</h2>
+      <p className={`${style.paragraph}`}>Everything you need to accept card payment and grow your business anywhere on the planet.</p>
     </div>
+    <div className={`${style.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button/>
+    </div>
+    </section>
   )
-}
 
 export default CTA
--- vite-project/src/components/Clients.jsx
+++ vite-project/src/components/Clients.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { clients } from '../constants'
+import style from "../style";
 
 const Clients = () => {
   return (
-    <div>
-      
-    </div>
+    <section className={`${style.flexCenter} my-4`}>
+      <div className={`${style.flexCenter} flex flex-wrap w-full `}>
+        {clients.map((clients) => (
+          <div key={clients.id} className={`flex-1 ${style.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+            <img src={clients.logo} alt="clients" className=' sm:w-[192px] w-[100px] object-contain' />
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
